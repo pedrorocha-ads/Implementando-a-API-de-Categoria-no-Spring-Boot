75dd1da68f5f19f179434f40bc025f9040cd4200
Pedro Rocha <161658584+pedrorocha-ads@users.noreply.github.com>
2025-03-10 15:30:42 -0300

    Criação da Classe Categoria

---
+++ demo/src/main/java/com/br/demo/model/Categoria.java
@@ -0,0 +1,44 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Categoria{
+
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    private String nome;
+
+    private String descricao;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
